0090741c82856d297ba4c2b8e91da5377d8fecdc
Gabriela Plantie <glplantie@gmail.com>
2020-10-21 18:56:17 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stepwise_formula",
-    version="0.0.3",
+    version="0.0.4",
     author="Gabriela Plantie",
     author_email="glplantie@gmail.com",
     description="Stepwise Formula",
