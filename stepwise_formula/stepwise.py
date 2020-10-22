4d9e28f46b8f8e39109dc607c3472bc1255dd88c
Gabriela Plantie <glplantie@gmail.com>
2020-10-21 21:20:32 -0300

    ready for predict

--- stepwise_formula/stepwise.py
+++ stepwise_formula/stepwise.py
@@ -12,8 +12,26 @@ def stepwise(formula, dataframe, model_type):
     for term in filter(lambda x: isinstance(x, Formula), f.terms()):
         dataframe[term.__repr__()] = term.apply(dataframe)
     
-    usedFields = map(lambda x: x.__repr__() if isinstance(x, Formula) else x ,f.terms())
+    usedFields = map(lambda x: x.__repr__() if isinstance(x, Formula) else x, f.terms())
     X = dataframe[usedFields]
     y = dataframe[y_name]
 
-    return backwardSelection(X,y, model_type=model_type)
+    backwardModel = backwardSelection(X,y, model_type=model_type)
+    return Model(backwardModel[2])
+
+
+class Model:
+    def __init__(self, model):
+        self.model = model
+        final_vars = ' + '.join(list(model.params.index[1:]))
+        self._formula = Formula.fromString(final_vars)
+
+    def predict(self, dataframe):
+        dataframe = dataframe.copy()
+        for term in filter(lambda x: isinstance(x, Formula), self._formula.terms()):
+            dataframe[term.__repr__()] = term.apply(dataframe)
+    
+        usedFields = map(lambda x: x.__repr__() if isinstance(x, Formula) else x, self._formula.terms())
+        X = dataframe[usedFields]
+        print(X.columns)
+        return self.model.predict(X) 
